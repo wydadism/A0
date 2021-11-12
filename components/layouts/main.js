6adc14fe91413e767d5d70499ca3e16696705d58
wydadism <94148495+wydadism@users.noreply.github.com>
2021-11-12 04:22:13 +0100

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -24,7 +24,7 @@ const Main = ({ children, router }) => {
         <meta name="twitter:site" content="@craftzdog" />
         <meta name="twitter:creator" content="@craftzdog" />
         <meta name="twitter:image" content="/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama's Homepage" />
+        <meta property="og:site_name" content="Remidi Kamal's Homepage" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/card.png" />
         <title>Takuya Matsuyama - Homepage</title>
